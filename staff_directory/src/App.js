2a78c073d4752167a88c8f11638da9e5c1815837
peacefrog <gregroyclark@gmail.com>
2020-06-25 16:27:42 -0500

    changes app div className to container

--- staff_directory/src/App.js
+++ staff_directory/src/App.js
@@ -6,7 +6,7 @@ import Navbar from "./components/NavBar";
 
 function App() {
   return (
-    <div className="App">
+    <div className="container">
       <Card />
       <Jumbotron />
       <NavBar />
