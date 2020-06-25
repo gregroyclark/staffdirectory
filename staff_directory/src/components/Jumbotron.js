923e1e3849ea6baae4143ae2d8b7b5eb5526eca2
peacefrog <gregroyclark@gmail.com>
2020-06-25 16:20:46 -0500

    adds Bootstrap navbar, card & jumbotron

---
+++ staff_directory/src/components/Jumbotron.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Jumbotron() {
+    return (
+        <div className="jumbotron">
+        <h1>Your Project</h1>
+        <p>
+            This is a Jumbotron.
+        </p>
+      </div>
+    )
+}
+
+export default Jumbotron;
