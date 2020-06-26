146f3f628d2be362a8f3e8c6cd4ae7424fc2d4d7
peacefrog <gregroyclark@gmail.com>
2020-06-26 09:04:50 -0500

    Updates

---
+++ precache-manifest.f1f47143a1eee4a187e1ad5bb817d073.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53e72241a1ee18b14fb980fdbf1fa4e9",
+    "url": "/staffdirectory/index.html"
+  },
+  {
+    "revision": "f54c2e900bddf12382f6",
+    "url": "/staffdirectory/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "0d6da0f80d0427fd79f6",
+    "url": "/staffdirectory/static/js/2.ed64f27b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/staffdirectory/static/js/2.ed64f27b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f54c2e900bddf12382f6",
+    "url": "/staffdirectory/static/js/main.e4b9ca73.chunk.js"
+  },
+  {
+    "revision": "8b6b549fcecaa2d80021",
+    "url": "/staffdirectory/static/js/runtime-main.6d1ed565.js"
+  }
+]);
